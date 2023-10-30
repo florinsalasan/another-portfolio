e7b5b7adceccebf361d94d3b243c1761f25088f0
Florin Salasan <florinsalasan@gmail.com>
2023-10-30 12:11:29 -0400

    began implementing modals for project descriptions, aligned items in header, polishing footer to have proper links and stylings

--- my-portfolio/src/components/subComponents/ProjectCard.tsx
+++ my-portfolio/src/components/subComponents/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import styles from "./ProjectCard.module.css";
 
 interface ProjectCardProps {
   title: string;
@@ -27,10 +28,10 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         sizes="100vw"
         style={{ width: "100%", height: "auto" }}
       />
-
-      <h3 className="text-xl font-semibold mb-2">{title}</h3>
-      <p className="text-gray-600 mb-4">{description}</p>
-
+      <h3 className="text-xl mb-2 text-gray-600">{title}</h3>
+      <p className={`${styles.cardDescription}text-gray-600 mb-4`}>
+        {description}
+      </p>
       <div className="flex flex-wrap">
         {technologies.map((tech, index) => (
           <span
@@ -41,7 +42,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           </span>
         ))}
       </div>
-
       <a
         href={link}
         target="_blank"
@@ -50,6 +50,35 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
       >
         View Project
       </a>
+      <div className={`${styles.modal} hidden modal`}>
+        <div className="modal-header">
+          <h3>{title}</h3>
+        </div>
+        <div className="modal-content">
+          <div className="modal-section">
+            <h4>Technologies Used</h4>
+            <ul>
+              {technologies.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="modal-section">
+            <h4>External Links</h4>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              Live Version
+            </a>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              GitHub Repository
+            </a>
+          </div>
+          <div className="modal-section">
+            <h4>In-Depth Description</h4>
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+      );
     </div>
   );
 };
