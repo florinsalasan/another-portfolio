df931edf2cd35cf99354b5882a5155667b0b2542
Florin Salasan <florinsalasan@gmail.com>
2024-02-15 11:19:17 -0500

    adding links to github to hopefully allow me to use templ files that are not in the same directory as main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/a-h/templ"
+    "github.com/labstack/echo/v4"
+    "github.com/florinsalasan/another-portfolio/components"
+)
+
+func main() {
+    app := echo.New()
+    app.GET("/", HomeHandler)
+    app.Logger.Fatal(app.Start(":4000"))
+}
+
+// Use a custom render to replace Echo's echo.Context.Render with 
+// templ's templ.Component.Render()
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+    ctx.Response().Writer.WriteHeader(statusCode)
+    ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+    return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
+func HomeHandler(c echo.Context) error {
+    return Render(c, http.StatusOK, Home())
+}
