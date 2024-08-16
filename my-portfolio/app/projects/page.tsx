c4e6395819174248940b180fed8daca8c5b89bdd
Florin Salasan <florinsalasan@gmail.com>
2024-08-16 04:59:29 -0400

    removed the [slug] directory in app/projects, I tried to reuse the slug format from the blog and I do not want to do that anymore

--- my-portfolio/app/projects/page.tsx
+++ my-portfolio/app/projects/page.tsx
@@ -1,7 +1,5 @@
 import ProjectCard from "../components/projectCard";
 import projectsData from "../../projects.json";
-import Link from "next/link";
-import DropdownChecklist from "../components/dropdownChecklist";
 
 interface ProjectData {
   title: string;
@@ -19,20 +17,14 @@ export default async function Page() {
       <div className="prose dark:prose-invert w-full max-w-none">
         <h1>Projects</h1>
         {typedProjectsData.map((project: ProjectData, index: number) => (
-          //<Link
-          //  href={`./projects/${project.title}`}
-          //  className="no-underline hover:underline"
-          //  key={crypto.randomUUID()}
-          //>
-            <ProjectCard
-              key={index}
-              imageUrl={project.imageUrl}
-              title={project.title}
-              description={project.description}
-              technologies={project.technologies}
-              gitLink={project.gitLink}
-            />
-          //</Link>
+          <ProjectCard
+            key={index}
+            imageUrl={project.imageUrl}
+            title={project.title}
+            description={project.description}
+            technologies={project.technologies}
+            gitLink={project.gitLink}
+          />
         ))}
         <p>
           *Thumbnails were made using Bing Copilot, or bad paintbrush skills or
@@ -42,31 +34,3 @@ export default async function Page() {
     </div>
   );
 }
-
-// import path from "path";
-// import matter from "gray-matter";
-// import fsPromises from "fs/promises";
-
-// export default async function ProjectsPage() {
-//   const filepath = path.join(process.cwd(), "projects");
-//   const projects = await fsPromises.readdir(filepath, { withFileTypes: false });
-//   let projectTags: string[] = [];
-//   const orderedPosts = projects.map((f) => {
-//     const d = matter.read(`./projects/${f}`);
-//     for (let tag of d.data.tags) {
-//       projectTags.push(tag);
-//     }
-//     return { data: d.data, slug: f.slice(0, -3) };
-//   });
-
-//   return (
-//     <div className="prose dark:prose-invert w-full">
-//       <h1>Posts</h1>
-//       <DropdownChecklist
-//         allTags={projectTags}
-//         orderedPosts={orderedPosts}
-//         type="projects"
-//       />
-//     </div>
-//   );
-// }
